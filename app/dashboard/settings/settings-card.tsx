73bf5d1a8ed100c04369cdc8d50c401056c5054c
Abdullah-Jammal <129687747+Abdullah-Jammal@users.noreply.github.com>
2024-06-29 16:28:16 +0300

    dashboard

---
+++ app/dashboard/settings/settings-card.tsx
@@ -0,0 +1,151 @@
+"use client"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Session } from "next-auth"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { SettingsSchema } from "@/types/settings-schema"
+import { Switch } from "@/components/ui/switch"
+import Image from "next/image"
+import { FormError } from "@/components/auth/form-error"
+import { FormSuccess } from "@/components/auth/form-success"
+import { useState } from "react"
+
+type SettingsForm = {
+  session : Session
+}
+
+export default function SettingsCard(session : SettingsForm) {
+
+  console.log(session)
+
+  const [error, setError] = useState<string | null>(null)
+  const [success, setSuccess] = useState<string | null>(null)
+  const [avatarUploading, setAvatarUploading] = useState(false)
+
+  const form = useForm<z.infer<typeof SettingsSchema>>({
+    resolver: zodResolver(SettingsSchema),
+    defaultValues: {
+      name: session.session.user?.name || undefined,
+      image: undefined,
+      // isTwoFactorEnabled : session.session.user?.isTwoFactorEnabled || false,
+      email : session.session.user?.email || undefined,
+      password: undefined,
+      newPassword: undefined
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof SettingsSchema>) {
+    console.log(values)
+  }
+
+  return (
+    <div className="container mx-auto mt-12">
+      <Card>
+        <CardHeader>
+          <CardTitle>Your Settings</CardTitle>
+          <CardDescription>Update your account settings</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>User Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="John Doe" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="image"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Avatar</FormLabel>
+                    <div className="flex">
+                      {!form.getValues('image') && (
+                        <div className="font-medium bg-primary rounded-full w-8 h-8 flex items-center justify-center">
+                          {session.session.user?.name?.charAt(0).toUpperCase()}
+                        </div>
+                      )}
+                      {form.getValues('image') && (
+                        <Image src={form.getValues('image')!} width={50} height={50} alt={form.getValues('name')!}/>
+                      )}
+                    </div>
+                    <FormControl>
+                      <Input placeholder="user image" type="hidden" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Your Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="********" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="newPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>New Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="********" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="isTwoFactorEnabled"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Two Factor Authentication</FormLabel>
+                    <FormDescription>This is your public display name.</FormDescription>
+                    <FormControl>
+                      <Switch className="scale-100" disabled={status === 'executing'}/>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* <FormError message={error}/>
+              <FormSuccess message={success}/> */}
+              <Button type="submit" disabled={status === 'executing' || avatarUploading}>Update Your Settings</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
--- components/navigation/user-button.tsx
+++ components/navigation/user-button.tsx
@@ -2,12 +2,83 @@
 
 import { Session } from "next-auth"
 import { signOut } from "next-auth/react"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarImage } from "@/components/ui/avatar"
+import Image from "next/image"
+import { AvatarFallback } from "@radix-ui/react-avatar"
+import { LogOut, Moon, Settings, Sun, TruckIcon } from "lucide-react"
+import { useTheme } from "next-themes"
+import { Switch } from "@/components/ui/switch"
+import { useState } from "react"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+
 
 export default function UserButton({user} : Session) {
+  const router = useRouter()
+  const {setTheme, theme} = useTheme()
+  const [check, setCheck] = useState(false)
+
+  function setSwitchState() {
+    switch(theme) {
+      case 'dark' : setCheck(true)
+      case 'light' : setCheck(false)
+      case 'system' : setCheck(false)
+    }
+  }
+
+  if(user)
   return (
-    <div>
-      {user?.email}
-      <button onClick={() => signOut()}>Sign Out</button>
-    </div>
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger>
+          <Avatar>
+            {user.image && (
+                <Image src={user.image} alt={user.name!} priority={true} fill={true} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
+            )}
+            {!user.image && (
+              <AvatarFallback className="bg-primary flex justify-center items-center w-full text-white">
+                <div className="font-bold">
+                  {user.name?.charAt(0).toUpperCase()}
+                </div>
+              </AvatarFallback>
+            )}
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="px-4 py-6 mr-2">
+          <DropdownMenuLabel className="p-3 mb-4 rounded-md">{user.email}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem  onClick={() => router.push('/dashboard/orders')} className="py-2 font-medium group cursor-pointer">
+            <TruckIcon size={16} className="mr-3 group-hover:translate-x-1 transition duration-300"/>My orders
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => router.push('/dashboard/settings')} className="py-2 font-medium group cursor-pointer"><Settings size={14} className="mr-3 group-hover:rotate-180 transition duration-500 ease-in-out"/>
+            Settings
+          </DropdownMenuItem>
+          <DropdownMenuItem className="py-2 font-medium cursor-pointer">
+            <div  onClick={(e) => e.stopPropagation()} className="flex justify-center items-center group">
+              <div className=" flex items-center justify-center">
+                <Sun size={14} className="mr-3 group-hover:rotate-180 absolute dark:scale-0 group-hover:text-yellow-400 transition-all duration-500 ease-in-out"/>
+                <Moon size={14} className="mr-3 group-hover:rotate-180 dark:scale-100 scale-0 group-hover:text-blue-400 transition-all duration-500 ease-in-out"/>
+              </div>
+              <p className="dark:text-blue-400 font-bold">{theme}</p>
+              <Switch className="scale-75 ml-1" checked={check} onCheckedChange={(e) => {
+                setCheck(pre => !pre)
+                if(e) setTheme('dark')
+                if(!e) setTheme('light')
+              }}/>
+            </div>
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => signOut()} className="py-2 bg-destructive flex items-center justify-center mt-3 font-bold hover:opacity-80 focus:bg-destructive transition duration-300 cursor-pointer">
+            <LogOut size={14} className="mr-3"/>
+            Sign out
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
   )
 }
